bbbc71f43a8d4ba8caaa4688f54d47fa5ec9d8a8
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-07-15 16:40:59 +0530

    feat(graphql): add Book and Author types with basic queries

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,6 +1,20 @@
+import Author from "../models/Author.js";
+import Book from "../models/Book.js";
+
 const resolvers = {
   Query: {
-    hello: () => "Hello from Graphql",
+    books: async () => await Book.find(),
+    book: async (_, { id }) => await Book.findById(id),
+    authors: async () => await Author.find(),
+    author: async (_, { id }) => await Author.findById(id),
+  },
+
+  Book: {
+    author: async (parent) => await Author.findById(parent.authorId),
+  },
+
+  Author: {
+    books: async (parent) => await Book.find({ authorId: parent.id }),
   },
 };
 
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,6 +1,23 @@
 const typeDefs = `
+    type Book {
+        id: ID!
+        name: String!
+        genre: String
+        author: Author
+    }
+
+    type Author {
+        id: ID!
+        name: String!
+        age: Int
+        books: [Book]
+    }
+
     type Query {
-        hello: String
+        books: [Book]
+        book(id: ID!): Book
+        authors: [Author]
+        author(id: ID!): Author
     }
 `;
 
